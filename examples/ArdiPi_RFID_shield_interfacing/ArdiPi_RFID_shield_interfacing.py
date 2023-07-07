8d06814d12b5fc5b8e9bbb24f57903b87e2c7f39
sbcshop3 <129948335+sbcshop3@users.noreply.github.com>
2023-07-07 11:44:51 +0530

    Update ArdiPi_RFID_shield_interfacing.py

--- examples/ArdiPi_RFID_shield_interfacing/ArdiPi_RFID_shield_interfacing.py
+++ examples/ArdiPi_RFID_shield_interfacing/ArdiPi_RFID_shield_interfacing.py
@@ -1,5 +1,5 @@
-from machine import UART, Pin      #import serial and pin access library
-import time    
+from machine import UART, Pin
+import time
 
 #Serial communication 
 RFID = UART(0,baudrate = 9600,tx = Pin(0),rx = Pin(1))
@@ -10,17 +10,16 @@ buzzer = machine.Pin(2, machine.Pin.OUT) #Define GPIO2 as output connected to bu
 #initially switch off by setting LOW value
 buzzer.value(0)
 relay.value(0)
-toggleVal = True
 
 while 1:
-    data_Read = RFID.readline(12)    #read RFID data byte
-    if data_Read is not None: # enter when RFID card detected
-        toggleVal = not toggleVal
+    data_Read = RFID.readline(12) #read data bytes comming from RFID Module, 12 Bytes for our card (variable for different card)
+    if data_Read is not None: # Enter when RFID card detected 
         print(data_Read)
-        data=data_Read.decode("utf-8") # convert raw data into utf-8 format
-        print(data)
-        relay.value(toggleVal) # Toggle Relay 
+        #data=data_Read.decode("utf-8") # convert raw data into utf-8 format
+        #print(data)
+        relay.value(1) # Switch ON Relay 
         buzzer.value(1) # Switch ON Buzzer 
-    time.sleep(0.2)    # wait 0.2 second
-    buzzer.value(0)    # Switch OFF Buzzer 
-
+        time.sleep(0.5)
+    time.sleep(0.5)
+    buzzer.value(0)
+    relay.value(0)
